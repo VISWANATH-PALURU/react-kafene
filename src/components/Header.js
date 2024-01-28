3600d54d33b1290a3c10e5cd8fe96a26a6b7b447
VISWANATH-PALURU <paluru.visu@gmail.com>
2024-01-28 19:28:13 +0530

    msg4

--- src/components/Header.js
+++ src/components/Header.js
@@ -19,7 +19,7 @@ const Header = ({loggedIn,setLoggedIn}) => {
                 <div className={styles.topbar}>
                     <div className={styles.topbar_LeftMenu}>
                         <div className={styles.topbar_logoWrapper}><img
-                            src="" alt="Logo" />
+                            src="https://edu-web-fundamentals.web.app/static/media/logo.58169365.png" alt="Logo" />
                             <p className={styles.topbar_brandName}>Kafene</p>
                         </div>
                         <nav>
