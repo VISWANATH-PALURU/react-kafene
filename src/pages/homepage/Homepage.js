69859507ee93b591426cbbd5f8fa4e31d59988bb
VISWANATH-PALURU <paluru.visu@gmail.com>
2024-01-28 15:04:21 +0530

    first commit

---
+++ src/pages/homepage/Homepage.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Login from '../../components/Login'
+
+
+
+const HomePage = () => {
+
+  return (
+    <div>
+
+      <Login/>
+
+      
+    </div>
+  )
+
+
+}
+
+export default HomePage;
---
+++ src/redux/reducers/index.js
@@ -0,0 +1,12 @@
+import { combineReducers } from "redux";
+import authReducer from "./authReducer";
+
+
+
+const rootReducer = combineReducers({
+    authentication : authReducer,
+})
+
+
+
+export default rootReducer;
